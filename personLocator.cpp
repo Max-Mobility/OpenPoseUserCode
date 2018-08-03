d06bea8840c8329ea3125f86c5511a17e5b35538
Dexter Watkins <dexter.a.watkins@vanderbilt.edu>
2018-08-02 23:37:17 -0500

    small commit to person locator

--- personLocator.cpp
+++ personLocator.cpp
@@ -55,22 +55,22 @@ DEFINE_double(render_threshold,         0.05,           "Only estimated keypoint
 DEFINE_double(alpha_pose,               0.6,            "Blending factor (range 0-1) for the body part rendering. 1 will show it completely, 0 will"
               " hide it. Only valid for GPU rendering.");
 
-int openPoseTutorialPose1()
+int personLocator()
 {
   try
     {
-      op::log("Starting OpenPose demo...", op::Priority::High);
+      op::log("Starting Person Locator...", op::Priority::High);
 
 
       // ------------------------- INITIALIZATION -------------------------
-      // Step 1 - Set logging level
+      // - Set logging level
       // - 0 will output all the logging messages
       // - 255 will output nothing
       op::check(0 <= FLAGS_logging_level && FLAGS_logging_level <= 255, "Wrong logging_level value.",
                 __LINE__, __FUNCTION__, __FILE__);
       op::ConfigureLog::setPriorityThreshold((op::Priority)FLAGS_logging_level);
       op::log("", op::Priority::Low, __LINE__, __FUNCTION__, __FILE__);
-      // Step 2 - Read Google flags (user defined configuration)
+      //  Read Google flags (user defined configuration)
       // outputSize
       const auto outputSize = op::flagsToPoint(FLAGS_output_resolution, "-1x-1");
       // netInputSize
@@ -85,7 +85,8 @@ int openPoseTutorialPose1()
                   __LINE__, __FUNCTION__, __FILE__);
       // Logging
       op::log("", op::Priority::Low, __LINE__, __FUNCTION__, __FILE__);
-      // Step 3 - Initialize all required classes
+
+      // Initialize all required classes
       op::ScaleAndSizeExtractor scaleAndSizeExtractor(netInputSize, outputSize, FLAGS_scale_number, FLAGS_scale_gap);
       op::CvMatToOpInput cvMatToOpInput{poseModel};
       op::CvMatToOpOutput cvMatToOpOutput;
@@ -93,8 +94,8 @@ int openPoseTutorialPose1()
       op::PoseCpuRenderer poseRenderer{poseModel, (float)FLAGS_render_threshold, !FLAGS_disable_blending,
           (float)FLAGS_alpha_pose};
       op::OpOutputToCvMat opOutputToCvMat;
-      op::FrameDisplayer frameDisplayer{"OpenPose Tutorial - Example 1", outputSize};
-      // Step 4 - Initialize resources on desired thread (in this case single thread, i.e. we init resources here)
+      op::FrameDisplayer frameDisplayer{"Person Locator", outputSize};
+      // Initialize resources on desired thread (in this case single thread, i.e. we init resources here)
       poseExtractorCaffe.initializationOnThread();
       poseRenderer.initializationOnThread();
 
@@ -122,8 +123,7 @@ int openPoseTutorialPose1()
         }
 
       // ------------------------- POSE ESTIMATION AND RENDERING -------------------------
-      // Step 1 - Read and load image, error if empty (possibly wrong path)
-      // Alternative: cv::imread(FLAGS_image_path, CV_LOAD_IMAGE_COLOR);
+      // Read and load image
       while(true){
         const auto timerBegin = std::chrono::high_resolution_clock::now();
 
@@ -133,25 +133,22 @@ int openPoseTutorialPose1()
         
         //convert to mat
         cv::Mat inputImage(cv::Size(640, 480), CV_8UC3, (void*)color.get_data(), cv::Mat::AUTO_STEP);
-        if(inputImage.empty())
-          op::error("Could not get camera stream image", __LINE__, __FUNCTION__, __FILE__);
         const op::Point<int> imageSize{inputImage.cols, inputImage.rows};
-        // Step 2 - Get desired scale sizes
+        // Get desired scale sizes
         std::vector<double> scaleInputToNetInputs;
         std::vector<op::Point<int>> netInputSizes;
         double scaleInputToOutput;
         op::Point<int> outputResolution;
-        std::tie(scaleInputToNetInputs, netInputSizes, scaleInputToOutput, outputResolution)
-          = scaleAndSizeExtractor.extract(imageSize);
-        // Step 3 - Format input image to OpenPose input and output formats
+        std::tie(scaleInputToNetInputs, netInputSizes, scaleInputToOutput, outputResolution) = scaleAndSizeExtractor.extract(imageSize);
+        // Format input image to OpenPose input and output formats
         const auto netInputArray = cvMatToOpInput.createArray(inputImage, scaleInputToNetInputs, netInputSizes);
         auto outputArray = cvMatToOpOutput.createArray(inputImage, scaleInputToOutput, outputResolution);
-        // Step 4 - Estimate poseKeypoints
+        // Estimate poseKeypoints
         poseExtractorCaffe.forwardPass(netInputArray, imageSize, scaleInputToNetInputs);
         const auto poseKeypoints = poseExtractorCaffe.getPoseKeypoints();
-        // Step 5 - Render poseKeypoints
+        // Render poseKeypoints
         poseRenderer.renderPose(outputArray, poseKeypoints, scaleInputToOutput);
-        // Step 6 - OpenPose output format to cv::Mat
+        // OpenPose output format to cv::Mat
         auto outputImage = opOutputToCvMat.formatToCvMat(outputArray);
 
         const auto now0 = std::chrono::high_resolution_clock::now();
@@ -178,5 +175,5 @@ int main(int argc, char *argv[])
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
   // Running openPoseTutorialPose1
-  return openPoseTutorialPose1();
+  return personLocator();
 }
